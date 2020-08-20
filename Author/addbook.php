4218ef41983c00abd87b1c743964b58b3c7c35f1
minahilh21 <minahilh21@gmail.com>
2020-08-20 20:04:50 +0500

    add book code

--- Author/addbook.php
+++ Author/addbook.php
@@ -1,96 +1,48 @@
 <?php
 session_start();
-$pageTitle='Add Book';
+$pageTitle="Add Book";
 ?>
 <html>
 <head>
-  <title><?php echo $pageTitle;?></title>
- <link rel="stylesheet" href="../css/style.css" type="text/css">
-  <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-
-     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+  <?php include('../inc/head.php');?>
 </head>
 <body>
-
   <div class="header">
-
     <div class="wrapper">
-
-      
-
       <ul class="nav">
         <li><a href="../index.php">HOME</a></li>
-    
-              </ul>
-
+      </ul>
     </div>
-
   </div>
- <section class="my-5">
-        <div class="py -5">
-       
-          <h2 class="text-center">Add Book</h2>
-        </div>
-        <div class="w-50 m-auto">
-          <form action="addbookAction.php" method="post" enctype="multipart/form-data">
-            <div class="form-group">
-             <label>Book Title</label>
-              <input type="text" name="title" autocomplete="off" required class="form-control">
-              
-
-            </div>
-             <div class="form-group">
-              <label>Author</label>
-              <input type="text" name="author" value="<?php echo $_SESSION['uName'];?>" autocomplete="off" required class="form-control">
-             
-              
-
-            </div>
-          <div class="form-group">
-             <label>Rating</label>
-              <input type="number" step="0.01" name="rating" autocomplete="off" min="1" max="5" required class="form-control">
-
-            </div>
-            <div class="form-group">
-              <label>Description</label>
-              <textarea type="text" name="des" autocomplete="off" required class="form-control"></textarea>
-             
-              
-
-            </div>
-          <div class="form-group">
-             <label>Upload File</label>
-              <input type="file" name="link" >
-
-            </div>
-          <div class="form-group">
-             
-            <button type="submit" class="btn btn-success">Add Book</button><br><br>
- <div class="form-group">
-            
-          </div>
-         
-          </form>
-
-        </div>
+  <section class="my-5">
+    <div class="py -5">
+     <h2 class="text-center">Add Book</h2>
+   </div>
+   <div class="w-50 m-auto">
+    <form action="addbookAction.php" method="post" enctype="multipart/form-data">
+      <div class="form-group">
+       <label>Book Title</label>
+       <input type="text" name="title" autocomplete="off" required class="form-control">
+       <div class="form-group">
+        <label>Description</label>
+        <textarea type="text"  rows="10" name="des" autocomplete="off" required class="form-control"></textarea>
+      </div>
+      <div class="form-group">
+       <label>Upload Book</label>
+       <input type="file" name="link" >
+     </div>
+     <div class="form-group">
+      <div class="form-group">
+       <label>Upload Cover Image</label>
+       <input type="file" name="image" >
+     </div>
+     <div class="form-group">
+      <button type="submit" class="btn btn-success">Add Book</button><br><br>
+      <div class="form-group">
+      </div>
+    </form>
+  </div>
 </section>
-
-
-
-
-
-
-
-
-
-
-     <script async src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-      <script async src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-      <script async src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
-
-      <?php include('../inc/footer.php');?>
+<?php include('../inc/footer.php');?>
 </body>
 </html>
--- Author/addbookAction.php
+++ Author/addbookAction.php
@@ -1,32 +1,24 @@
 <?php
 session_start();
 include('../inc/connection.php');
-
 $t=$_REQUEST['title'];
 $a=$_SESSION['uName'];
-$r=$_REQUEST['rating'];
 $d=addslashes ($_REQUEST['des']);
-
 $name= $_FILES['link']['name'];
-
+$image= $_FILES['image']['name'];
 $tmp_name= $_FILES['link']['tmp_name'];
-if (isset($name)) 
-{
-$path= '/var/www/my-domain/project/uploads/';
-$path_name=$path.$name;
-if (!empty($name))
-{   
-    if (move_uploaded_file($tmp_name, $path.$name)) 
-    {
-        echo 'Uploaded!';
-        $sql = "INSERT INTO books(title,author,rating,des,published,link) VALUES ('$t','$a','$r','$d','0','$path_name')";
-$conn->query($sql);
-$conn->close();
-
-
+$tmp_image= $_FILES['image']['tmp_name'];
+if (isset($name) && isset($image)) {
+  $path= '/var/www/my-domain/project/uploads/';
+  $path_name=$path.$name;
+  $path_image=$path.$image;
+  if (!empty($name)&& !empty($image)){   
+    if (move_uploaded_file($tmp_name, $path.$name) && move_uploaded_file($tmp_image, $path.$image)){
+      $sql = "INSERT INTO books(title,author,rating,des,published,link,image) VALUES ('$t','$a','0','$d','0','$path_name','$path_image')";
+      $db=new db();
+      $db->query($sql);
     }
-}
+  }
 }
 header("location:Author.php");
-
 ?>
